ec52f2cb092e8f494c30e9e3fcbdc609183fa394
“Alexandre <alex@thecoursebook.com>
2013-01-29 21:58:11 -0800

    .

--- lib/omniauth/strategies/box.rb
+++ lib/omniauth/strategies/box.rb
@@ -57,6 +57,7 @@ puts "ACCESS TOKEN: >>>>>>>>>>>>>>>> #{access_token.inspect}"
 puts "ACCESS_TOKEN.TOKEN >>>>>>>>>>>>>>>> #{access_token.token.inspect}"
         token = access_token.token
         access_token.params["grant_type"] = "authorization_code"
+        access_token.params["code"] = request.params['code']
 puts "TOKEN >>>>>>>>>>>>>>>>> #{token.inspect}"
 puts "ACCESS_TOKEN.params >>>>>>>>>>>>>>>>> #{access_token.params.inspect}"
         @access_token = ::OAuth2::AccessToken.new(client, token, access_token.params)
